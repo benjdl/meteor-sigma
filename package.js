a33731fab57b06d6265e40fb506d6a9c1298f26f
Benjamin DELORME <b.delorme@wcie.fr>
2015-12-04 15:44:40 +0100

    Fix meteor account name & directory path.

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: "benjdl:sigma",
+  name: "benj:sigma",
   summary: "Sigma is a library dedicated to graph drawing.",
   version: "1.0.3",
   git: "https://github.com/benjdl/meteor-sigma"
@@ -8,18 +8,18 @@ Package.describe({
 Package.onUse(function(api) {
 
   api.addFiles([
-    "benjdl:sigma/sigma.min.js",
-    "benjdl:sigma/plugins/sigma.exporters.svg.js",
-    "benjdl:sigma/plugins/sigma.layout.forceAtlas2.min.js",
-    "benjdl:sigma/plugins/sigma.parsers.cypher.js",
-    "benjdl:sigma/plugins/sigma.parsers.gexf.min.js",
-    "benjdl:sigma/plugins/sigma.parsers.json.min.js",
-    "benjdl:sigma/plugins/sigma.plugins.animate.min.js",
-    "benjdl:sigma/plugins/sigma.plugins.dragNodes.min.js",
-    "benjdl:sigma/plugins/sigma.plugins.filter.min.js",
-    "benjdl:sigma/plugins/sigma.plugins.neighborhoods.min.js",
-    "benjdl:sigma/plugins/sigma.renderers.customShapes.min.js",
-    "benjdl:sigma/plugins/sigma.renderers.snapshot.js"
-    "benjdl:sigma/plugins/sigma.statistics.HITS.min.js"
+    "sigma/sigma.min.js",
+    "sigma/plugins/sigma.exporters.svg.js",
+    "sigma/plugins/sigma.layout.forceAtlas2.min.js",
+    "sigma/plugins/sigma.parsers.cypher.js",
+    "sigma/plugins/sigma.parsers.gexf.min.js",
+    "sigma/plugins/sigma.parsers.json.min.js",
+    "sigma/plugins/sigma.plugins.animate.min.js",
+    "sigma/plugins/sigma.plugins.dragNodes.min.js",
+    "sigma/plugins/sigma.plugins.filter.min.js",
+    "sigma/plugins/sigma.plugins.neighborhoods.min.js",
+    "sigma/plugins/sigma.renderers.customShapes.min.js",
+    "sigma/plugins/sigma.renderers.snapshot.js",
+    "sigma/plugins/sigma.statistics.HITS.min.js"
   ], ["client"]);
 });
